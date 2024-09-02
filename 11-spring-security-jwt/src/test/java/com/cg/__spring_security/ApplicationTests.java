acd15afb761b13d987ca7d9850fc43e9f7b6cfaf
unknown <albinxavier@gmail.com>
2024-09-02 14:59:18 +0530

    first commit

---
+++ 11-spring-security-jwt/src/test/java/com/cg/__spring_security/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.__spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
